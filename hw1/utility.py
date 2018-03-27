42ea212499e47381ac2a9a76d90db80e9a2e0954
pixiyM <wangzx1994@gmail.com>
2018-03-28 01:10:09 +0800

    update new

--- hw1/utility.py
+++ hw1/utility.py
@@ -108,6 +108,65 @@ class CNN_Net1(nn.Module):
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
 
+# CNN model2
+class CNN_Net2(nn.Module):
+    def __init__(self):
+        super(Net2, self).__init__()
+        self.conv1 = nn.Conv2d(1, 2, kernel_size=5)
+        self.conv1_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(288, 30)
+        self.fc2 = nn.Linear(30, 10)
+    
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1_drop(self.conv1(x)), 2))
+        x = x.view(-1, 288)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+    
+# CNN model3
+class CNN_Net3(nn.Module):
+    def __init__(self):
+        super(Net3, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 5, kernel_size=3)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(125, 63)
+        self.fc2 = nn.Linear(63, 10)
+    
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 125)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+
+# CNN model4
+class CNN_Net4(nn.Module):
+    def __init__(self):
+        super(Net4, self).__init__()
+        self.conv1 = nn.Conv2d(1, 20, kernel_size=5)
+        self.conv2 = nn.Conv2d(20, 15, kernel_size=3)
+        self.conv3 = nn.Conv2d(15, 10, kernel_size=2)
+        self.conv3_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(40, 80)
+        self.fc2 = nn.Linear(80, 21)
+        self.fc3 = nn.Linear(21, 10)
+    
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2(x), 2))
+        x = F.relu(F.max_pool2d(self.conv3_drop(self.conv3(x)), 2))
+        x = x.view(-1, 40)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc3(x)
+        return F.log_softmax(x, dim=1)
+    
 """
 TODO: Generate the function data
 """
@@ -142,6 +201,7 @@ def fetch_mnist_data(dir):
     return train_dataset, test_dataset
 
 """
+TODO: Calculate model parameters number
 """
 def model_params(net):
     model_parameters = filter(lambda p: p.requires_grad, net.parameters())
